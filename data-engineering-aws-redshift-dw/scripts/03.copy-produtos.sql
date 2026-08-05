138f05eba009af527b1c62e7bc6a84ccc2ab3bc4
heulersilva <heulersilva@yahoo.com.br>
2026-08-05 20:09:25 -0300

    feat(ingestion): add COPY scripts for S3-to-Redshift data loading

---
+++ data-engineering-aws-redshift-dw/scripts/03.copy-produtos.sql
@@ -0,0 +1,7 @@
+copy produtos
+from 's3://egalimentos-sales/data/produtos.csv'
+CREDENTIALS 'aws_access_key_id=<YOUR_AWS_ACCESS_KEY_ID>;aws_secret_access_key=<YOUR_AWS_SECRET_ACCESS_KEY>'
+region 'sa-east-1'
+delimiter ';'
+IGNOREHEADER 1
+DATEFORMAT 'DD/MM/YYYY';
---
+++ data-engineering-aws-redshift-dw/scripts/04.copy-clientes.sql
@@ -0,0 +1,7 @@
+copy clientes
+from 's3://egalimentos-sales/data/clientes.csv'
+CREDENTIALS 'aws_access_key_id=<YOUR_AWS_ACCESS_KEY_ID>;aws_secret_access_key=<YOUR_AWS_SECRET_ACCESS_KEY>'
+region 'sa-east-1'
+delimiter ';'
+IGNOREHEADER 1
+DATEFORMAT 'DD/MM/YYYY';
